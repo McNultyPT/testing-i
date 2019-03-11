80062789b4e229e3804bbff8de41d80fd960853b
McNultyPT <mcnultypt@gmail.com>
2019-03-11 13:37:26 -0700

    working on repair function

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -14,9 +14,34 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  let items = {
+    name: 'item',
+    durability: 50,
+    enhancement: 20
+  };
+
+  const newItem = Object.keys(items).map(item => {
+    item.durability = 100;
+  });
+  
+  return { newItem };
 }
 
 function get(item) {
   return { ...item };
 }
+
+// - Items -
+// - Items have name, durability and enhancement.
+// - The item's enhancement it's a number from 0 to 20.
+// - The item's durability it's a number from 0 to 100.
+
+// - When enhancement succeeds -
+// - The item's enhancement increases by 1.
+// - If the item enhancement level is 20, the enhancement level is not changed.
+// - The durability of the item is not changed.
+
+// - When enhancement fails -
+// - If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// - If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// - If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,7 @@
 const enhancer = require('./enhancer.js');
-// initial push
+
+describe('repair()', () => {
+    it('should return a durability of 100', (item) => {
+        expect(enhancer.repair(item.durability)).toBe(100);
+    })
+});
